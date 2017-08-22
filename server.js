11d8522d9f85bbeb90916fecac84f73e32b3c194
Andrew Gibson <andrewgib14@gmail.com>
2017-08-22 14:55:20 -0500

    populated home page

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const mustacheExpress = require("mustache-express");
 const mongo = require("mongodb");
 const logger = require("morgan");
 const path = require("path");
-const data = require("./data.js")
 const MongoClient = mongo.MongoClient;
 const dbUrl = "mongodb://localhost:27017/mongoRobots";
 const port = process.env.PORT || 8000;
+const ObjectId = mongo.ObjectID;
 const app = express();
+let Robots;
+let DB;
 
 
 app.engine("mustache", mustacheExpress());
@@ -15,6 +17,15 @@ app.set("views", "./views");
 app.set("view engine", "mustache");
 
 app.use(express.static(path.join(__dirname, "./public")));
+
+MongoClient.connect(dbUrl, (err, db) => {
+    if (err) {
+        return console.log("Error connecting to the database:", err);
+    }
+
+    DB = db;
+    Robots = db.collection("robots");
+});
 
 app.get("/insertMany", function (req, res) {
     MongoClient.connect(dbUrl, function (err, db) {
@@ -29,6 +40,26 @@ app.get("/insertMany", function (req, res) {
             res.send(savedRobots);
             db.close();
         })
+    })
+})
+
+app.get("/", (req, res) => {
+    Robots.find({}).toArray((err, foundRobots) => {
+        if (err) res.status(500).send(err);
+
+        res.render("home", { users: foundRobots });
+    });
+});
+
+app.get("/:_id", function (req, res) {
+    Robots.findOne({ _id: ObjectId(req.params._id) }, (err, foundRobot) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+        else if (!foundRobot) {
+            res.send("No user found")
+        }
+        res.render("profile", { data: foundRobot });
     })
 })
 
